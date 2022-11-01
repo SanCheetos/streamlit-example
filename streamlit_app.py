4277fcefb765e3180b2867d49b1bd0ea20403861
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2022-11-01 18:16:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 with st.echo(code_location='below'):
     file = st.file_uploader(label="hello")
-    temp = tempfile.NamedTemporaryFile(mode="wd")
+    temp = tempfile.NamedTemporaryFile(mode="wb")
     bytes_data = files.getvalue()
     temp.write(bytes_data)
     
